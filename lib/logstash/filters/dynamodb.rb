67b863d4182dee956fdb47bb1c4a2d4c3d717270
Ahmed Ammar <aammar@genesi-usa.com>
2016-02-28 15:08:56 +0200

    parse oldImage when event is "REMOVE"

--- lib/logstash/filters/dynamodb.rb
+++ lib/logstash/filters/dynamodb.rb
@@ -14,42 +14,40 @@ require 'json'
 
 class LogStash::Filters::DynamoDB < LogStash::Filters::Base
 
-
-
 class Unmarshaler
-        def format(obj)
-          type, value = extract_type_and_value(obj)
-          case type.downcase
-          when :m
-            value.each.with_object({}) do |(k, v), map|
-              map[k.to_s] = format(v)
-            end
-          when :l then value.map { |v| format(v) }
-          when :s then value
-          when :n then BigDecimal.new(value)
-          when :b then StringIO.new(value)
-          when :null then nil
-          when :bool then value
-          when :ss then Array.new(value)
-          when :ns then Array.new(value.map { |n| BigDecimal.new(n) })
-          when :bs then Array.new(value.map { |b| StringIO.new(b) })
-          else
-            raise ArgumentError, "unhandled type #{type.inspect}"
-          end
-        end
+  def format(obj)
+    type, value = extract_type_and_value(obj)
+    case type.downcase
+    when :m
+      value.each.with_object({}) do |(k, v), map|
+        map[k.to_s] = format(v)
+      end
+    when :l then value.map { |v| format(v) }
+    when :s then value
+    when :n then BigDecimal.new(value)
+    when :b then StringIO.new(value)
+    when :null then nil
+    when :bool then value
+    when :ss then Array.new(value)
+    when :ns then Array.new(value.map { |n| BigDecimal.new(n) })
+    when :bs then Array.new(value.map { |b| StringIO.new(b) })
+    else
+      raise ArgumentError, "unhandled type #{type.inspect}"
+    end
+  end
 
-        def extract_type_and_value(obj)
-          case obj
-          when Hash then obj.to_a.first
-          when Struct
-            obj.members.each do |key|
-              value = obj[key]
-              return [key, value] unless value.nil?
-            end
-          else
-            raise ArgumentError, "unhandled type #{obj.inspect}"
-          end
-        end
+  def extract_type_and_value(obj)
+    case obj
+    when Hash then obj.to_a.first
+    when Struct
+      obj.members.each do |key|
+        value = obj[key]
+        return [key, value] unless value.nil?
+      end
+    else
+      raise ArgumentError, "unhandled type #{obj.inspect}"
+    end
+  end
 end
 
 
@@ -66,21 +64,29 @@ end
 
   public
   def filter(event)
-	doc = JSON.parse(event['message'], :symbolize_names => true)
+    doc = JSON.parse(event['message'], :symbolize_names => true)
 
-	event['eventName'] = doc[:eventName]
+    event['eventName'] = doc[:eventName]
 
-	keys = {}
-	doc[:dynamodb][:keys].each do |key, value|
-		keys[key.to_s] = @unmarshaller.format(value)
-	end
-	event['keys'] = keys
+    keys = {}
+    doc[:dynamodb][:keys].each do |key, value|
+      keys[key.to_s] = @unmarshaller.format(value)
+    end
+    event['keys'] = keys
 
-	if doc[:dynamodb][:newImage]
-		doc[:dynamodb][:newImage].each do |key, value|
-			event[key.to_s] = @unmarshaller.format(value)
-		end
-	end
+    if event['eventName'] == "REMOVE"
+      if doc[:dynamodb][:oldImage]
+        doc[:dynamodb][:oldImage].each do |key, value|
+          event[key.to_s] = @unmarshaller.format(value)
+        end
+      end
+    else
+      if doc[:dynamodb][:newImage]
+        doc[:dynamodb][:newImage].each do |key, value|
+          event[key.to_s] = @unmarshaller.format(value)
+        end
+      end
+    end
 
     filter_matched(event)
   end
